76565e6cb2ea21272df01df7c0804ca7b37b94ab
Gwendolyn <wilkistagwendolyn@gmail.com>
2025-06-01 22:05:52 +0300

    Project scaffold

--- interfaces/index.ts
+++ interfaces/index.ts
@@ -17,3 +17,33 @@ export interface ButtonProps {
   disabled?: boolean;
   type?: 'button' | 'submit';
 }
+
+export interface PropertyProps {
+  name: string;
+  address: {
+    state: string;
+    city: string;
+    country: string;
+  };
+  rating: number;
+  category: string[];
+  price: number;
+  offers: {
+    bed: string;
+    shower: string;
+    occupants: string;
+  };
+  image: string;
+  discount: string;
+}
+
+export interface FooterLinksProps {
+  title: string;
+  links: {
+    first: string;
+    second: string;
+    third: string;
+    fourth?: string;
+    fifth?: string;
+  }
+}
